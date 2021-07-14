a85695209b3b49a962849c4490cbd63853a8f2de
Башкин Владислав Владимирович <VVBashkin@sberbank.ru>
2021-07-14 09:35:40 +0400

    Initial commit

---
+++ src/main/java/com/magenta/distributioncenter/MagentaDistributionCenterApplication.java
@@ -0,0 +1,13 @@
+package com.magenta.distributioncenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagentaDistributionCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MagentaDistributionCenterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/magenta/distributioncenter/MagentaDistributionCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magenta.distributioncenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagentaDistributionCenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
